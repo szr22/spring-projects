b29811d5a3f9556d34acbdc6ed261e5d807818b3
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-02 17:03:20 -0400

    singleton vs prototype scope

--- conference/src/main/java/AppConfig.java
+++ conference/src/main/java/AppConfig.java
@@ -2,13 +2,17 @@ import com.prettyye.repository.HibernateSpeakerRepositoryImpl;
 import com.prettyye.repository.SpeakerRepository;
 import com.prettyye.service.SpeakerService;
 import com.prettyye.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+//    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
 //        service.setRepository(getSpeakerRepository());
--- conference/src/main/java/Application.java
+++ conference/src/main/java/Application.java
@@ -8,6 +8,12 @@ public class Application {
 
         SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
+        System.out.println(service);
+
         System.out.println(service.findAll().get(0).getFirstName());
+
+        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(service2);
     }
 }
