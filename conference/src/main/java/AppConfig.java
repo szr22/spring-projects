5c155d554619229fdaf7d99fa7f3fecffc027535
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-02 15:38:13 -0400

    constructor injection

--- conference/src/main/java/AppConfig.java
+++ conference/src/main/java/AppConfig.java
@@ -10,8 +10,8 @@ public class AppConfig {
 
     @Bean(name = "speakerService")
     public SpeakerService getSpeakerService() {
-        SpeakerServiceImpl service = new SpeakerServiceImpl();
-        service.setRepository(getSpeakerRepository());
+        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+//        service.setRepository(getSpeakerRepository());
         return service;
     }
 
