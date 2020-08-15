01f611328c52b72add990fa3878e01bce9911c3f
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-15 11:37:02 -0400

    fix issue and update connection setting

--- conference-demo/src/main/java/com/prettyye/conferencedemo/controller/SessionsController.java
+++ conference-demo/src/main/java/com/prettyye/conferencedemo/controller/SessionsController.java
@@ -2,6 +2,7 @@ package com.prettyye.conferencedemo.controller;
 
 import com.prettyye.conferencedemo.models.Session;
 import com.prettyye.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -29,5 +30,19 @@ public class SessionsController {
     @ResponseStatus(HttpStatus.CREATED)
     public Session create(@RequestBody final Session session){
         return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // Also need to check for children records before deleting.
+        sessionRepository.deleteById(id);
+    }
+
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what
+        // TODO: Add validation that all attributes are passed in, otherwise return 400 bad payload
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 }
