2afaf33d50fe5f006849762be5c00872eef1c362
Zhenrong Shi <shizhenrong1987@hotmail.com>
2020-08-15 13:01:05 -0400

    update

--- conference-demo/src/main/java/com/prettyye/conferencedemo/models/Speaker.java
+++ conference-demo/src/main/java/com/prettyye/conferencedemo/models/Speaker.java
@@ -1,11 +1,14 @@
 package com.prettyye.conferencedemo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Speaker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +22,7 @@ public class Speaker {
 
     @Lob
     @Type(type = "org.hibernate.type.BinaryType")
+    @JsonIgnore
     private byte[] speaker_photo;
 
     @ManyToMany(mappedBy = "speakers")
@@ -33,5 +37,61 @@ public class Speaker {
 
     public void setSpeaker_photo(byte[] speaker_photo) {
         this.speaker_photo = speaker_photo;
+    }
+
+    public long getSpeaker_id() {
+        return speaker_id;
+    }
+
+    public void setSpeaker_id(long speaker_id) {
+        this.speaker_id = speaker_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getSpeaker_bio() {
+        return speaker_bio;
+    }
+
+    public void setSpeaker_bio(String speaker_bio) {
+        this.speaker_bio = speaker_bio;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
     }
 }
